db129a4432ea4cae2635c4f9b01d81634465927f
MyUsername <my@email.com>
2021-10-21 15:54:20 +0200

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let km = prompt ("Quanti Km devi percorrere?");
+
+let age = prompt ("Quanti anni hai?");
+
+const priceKm = ("0.21");
+
+const euro = ("\u20AC");
+
+let price = km * priceKm + euro;
+
+if (age < 18) {
+    const calculateDiscount = ((price) * (20) / (100));
+} else if (age > 65) {
+    const calculateDiscount = ((price) * (40) / (100));
+} else {
+    const calculateDiscount = (price);
+}
+
+if (age < 18) {
+    getElementById("price").innerHTML = `Il prezzo del biglietto è: ${price} + ${calculateDiscount}`;
+} else if (age > 65) {
+    getElementById("price").innerHTML = `Il prezzo del biglietto è: ${price} + ${calculateDiscount}`;
+} else {
+    getElementById("price").innerHTML = `Il prezzo del biglietto è: ${price}`;
+}
+
+// document.getElementById("price").innerHTML = `Il prezzo del biglietto è: ${price} + ${calculateDiscount}`;
